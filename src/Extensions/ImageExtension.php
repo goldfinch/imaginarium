c57dacd4b1472ccf84f2a36126cac4c0e6043c90
Art Karasev <as.karasyov@gmail.com>
2023-08-08 15:57:13 +1200

    responsive image

--- src/Extensions/ImageExtension.php
+++ src/Extensions/ImageExtension.php
@@ -2,11 +2,15 @@
 
 namespace Goldfinch\Imaginarium\Extensions;
 
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\Core\Extension;
+use SilverStripe\View\ArrayData;
 
 class ImageExtension extends Extension
 {
     private static $db = [];
+
+    private $RIOptions = null;
 
     public function LazyFocusFill(int $width, int $height)
     {
@@ -33,9 +37,212 @@ class ImageExtension extends Extension
         return $this->Lazy($this->owner->FocusCropHeight($height));
     }
 
-    public function LazyThumb(int $width, int $height)
+    public function getSourceMediaQuery($mq, $w)
     {
-        return $this->Lazy($this->owner->FocusFill($width, $height));
+        $queryString = '';
+
+        $mq = trim($mq);
+
+        return str_replace([
+          '%w'
+        ], [
+          $w.'px'
+        ], $mq);
+    }
+
+    private function setRIoptions($options)
+    {
+        if ($options)
+        {
+            $this->RIOptions = json_decode($options, true);
+        }
+    }
+
+    private function getRIoptions()
+    {
+        return $this->RIOptions;
+    }
+
+    private function hasRIOption($prop)
+    {
+        $options = $this->getRIoptions();
+
+        if ($options && isset($options[$prop]))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getRIOption($prop)
+    {
+        $options = $this->getRIoptions();
+
+        if ($options && isset($options[$prop]))
+        {
+            return $options[$prop];
+        }
+
+        return null;
+    }
+
+    /**
+     * intrinsicWidth & intrinsicHeight should be the highest possible size (the top breakpoint) of the current image
+     */
+    public function ResponsiveImage($intrinsicWidth, $intrinsicHeight, $options = null)
+    {
+        $this->setRIoptions($options);
+        // ! here to make sure that the initially passed $options is not carring options, to force using object property instead
+        $options = null;
+
+        // Bootstrap var --bs-gutter-x
+        if ($this->hasRIOption('gutter') && $this->getRIOption('gutter'))
+        {
+            $customGutter = $this->getRIOption('gutter');
+        }
+
+        $intrinsicRatio = $intrinsicWidth / $intrinsicHeight;
+
+        // ? default
+        // !important keep the descending order : from largest to smallest
+        $breakpoints = [
+          'xl' => 1210,
+          'lg' => 992,
+          'md' => 768,
+          'sm' => 576,
+          // 'xs' => 0, // ! need here as a started point
+        ];
+
+        $breakpointsMax = [
+          'xl' => 1174,
+          'lg' => 960,
+          'md' => 720,
+          'sm' => 540,
+          // 'xs' => 0, // ! need here as a started point
+        ];
+
+        $gutters = [
+          'xl' => 12,
+          'lg' => 12,
+          'md' => 12,
+          'sm' => 12,
+          // 'xs' => 0, // ! need here as a started point
+        ];
+
+        // ? default
+        // !important keep the descending order : from largest to smallest
+        $mediaQueries = [
+          'xl' => ['(min-width: %w) and (min-device-pixel-ratio: 2.0)', '(min-width: %w)'],
+          'lg' => ['(min-width: %w) and (min-device-pixel-ratio: 2.0)', '(min-width: %w)'],
+          'md' => ['(min-width: %w) and (min-device-pixel-ratio: 2.0)', '(min-width: %w)'],
+          'sm' => ['(min-width: %w) and (min-device-pixel-ratio: 2.0)', '(min-width: %w)'],
+          // 'xs' => [], // ! keep empty
+        ];
+
+        if (isset($customGutter) && $customGutter)
+        {
+            // with custom global gutters (for all)
+            $scaling = current($breakpointsMax) - ($customGutter * 2);
+        }
+        else if (isset($customGutter) && $customGutter === false)
+        {
+            // without gutters
+            $scaling = false;
+        }
+        else
+        {
+            // base gutters (default)
+            $scaling = current($breakpointsMax) - (current($gutters) * 2);
+        }
+
+        if ($scaling && $scaling != $intrinsicWidth)
+        {
+            $scalingRatio = $scaling / $intrinsicWidth;
+        }
+
+        $sizes = ArrayList::create();
+
+        foreach($breakpoints as $bp => $w)
+        {
+            $mqImageW = $breakpointsMax[$bp]; // $w
+
+            $mqImageW -= ($gutters[$bp] * 2); // $w
+
+            // << Modifications
+
+            if (isset($scalingRatio) && (!isset($customGutter) || $customGutter !== false))
+            {
+                // with custom global gutters (for all) && base gutters (default)
+                $mqImageW = round($mqImageW / $scalingRatio);
+            }
+
+            // Modifications >>
+
+            if (isset($mediaQueries[$bp]))
+            {
+                foreach($mediaQueries[$bp] as $mq)
+                {
+                    if ($this->hasRIOption('bpw'))
+                    {
+                        if (isset($this->getRIOption('bpw')[$bp]))
+                        {
+                            $mqImageW = $this->getRIOption('bpw')[$bp];
+                        }
+                    }
+
+                    $mqImageH = round($mqImageW / $intrinsicRatio);
+
+                    if ($this->hasRIOption('bph'))
+                    {
+                        if (isset($this->getRIOption('bph')[$bp]))
+                        {
+                            $mqImageH = $this->getRIOption('bph')[$bp];
+                        }
+                    }
+
+                    $sizes->push(ArrayData::create([
+                      'Breakpoint' => $bp,
+                      'Image' => $this->owner->FocusFill($mqImageW, $mqImageH),
+                      'MediaQuery' => $this->getSourceMediaQuery($mq, $w),
+                      // Scale
+                    ]));
+                }
+            }
+        }
+
+        // Default image (the max width before the first specified breakpoint), it is assumed that below the first breakpoint, the image is treated as width: 100% within its container
+        $defaultWidth = (end($breakpoints) - 1);
+
+        // Check if custom xs supplied, and use it instead if so
+        if ($this->hasRIOption('bpw'))
+        {
+            if (isset($this->getRIOption('bpw')['xs']))
+            {
+                $defaultWidth = $this->getRIOption('bpw')['xs'];
+            }
+        }
+
+        $defaultHeight = round($defaultWidth / $intrinsicRatio);
+
+        if ($this->hasRIOption('bph'))
+        {
+            if (isset($this->getRIOption('bph')['xs']))
+            {
+                $defaultHeight = $this->getRIOption('bph')['xs'];
+            }
+        }
+
+        // Placeholder image
+
+        $placeholderWidth = 80;
+        $placeholderHeight = round($placeholderWidth / $intrinsicRatio);
+
+        return $this->owner->customise([
+          'Sizes' => $sizes,
+          'DefaultImage' => $this->owner->FocusFill($defaultWidth, $defaultHeight),
+          'DefaultImagePlaceholder' => $this->owner->FocusFill($placeholderWidth, $placeholderHeight),
+        ])->renderWith(['type' => 'Includes', 'ResponsiveImage']);
     }
 
     protected function Lazy($file)
