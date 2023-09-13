0feeede06a50adfd71a4a87e92e93210bf0e7993
Art Karasev <as.karasyov@gmail.com>
2023-09-13 12:07:32 +1200

    tweaks

--- src/Services/Compressor.php
+++ src/Services/Compressor.php
@@ -5,6 +5,7 @@ namespace Goldfinch\Imaginarium\Services;
 use ReflectionMethod;
 use ShortPixel\ShortPixel;
 use App\Models\ImageVariant;
+use foroco\BrowserDetection;
 use SilverStripe\Assets\Image;
 use ShortPixel\AccountException;
 use function ShortPixel\fromUrls;
@@ -25,6 +26,79 @@ class Compressor
     protected $client;
     protected $compressor;
     protected $options;
+
+    /**
+     * Detecting browser and checking if its version support requested format
+     */
+    public static function browserCheck($format)
+    {
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        $browserDetecter = new BrowserDetection();
+        $userAgen = $browserDetecter->getAll($agent);
+
+        // https://caniuse.com/avif
+        // https://caniuse.com/webp
+
+        if ($userAgen)
+        {
+            $browsers = [
+                'Chrome' => [
+                    'avif' => 85,
+                    'webp' => 32,
+                ],
+                'Edge' => [
+                    'avif' => false,
+                    'webp' => 18,
+                ],
+                'Safari' => [
+                    'avif' => 16.4,
+                    'webp' => 16,
+                ],
+                'Firefox' => [
+                    'avif' => 93,
+                    'webp' => 65,
+                ],
+                'Opera' => [
+                    'avif' => 71,
+                    'webp' => 19,
+                ],
+                'IE' => [
+                    'avif' => false,
+                    'webp' => false,
+                ],
+            ];
+
+            if (isset($browsers[$userAgen['browser_name']]))
+            {
+                $brw = $browsers[$userAgen['browser_name']];
+
+                if (isset($brw[$format]) && $brw[$format] !== false)
+                {
+                    return $userAgen['browser_version'] >= $brw[$format];
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static function imageSupport()
+    {
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        $support = [];
+
+        if (strpos($agent, 'image/avif') !== false || self::browserCheck('avif'))
+        {
+            $support[] = 'avif';
+        }
+
+        if (strpos($agent, 'image/webp') !== false || self::browserCheck('webp'))
+        {
+            $support[] = 'webp';
+        }
+
+        return $support;
+    }
 
     public static function storeCompression($record)
     {
